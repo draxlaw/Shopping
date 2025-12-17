155c7f287202f00fb80635a9b4fd439ae74e7ca0
walbe <walyise@gmail.com>
2025-12-17 14:01:24 +0000

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+const app = express();
+
+
+app.get("/api/health", (req,res) =>{
+   
+    res.status(200).json({ message: "Success"})
+})
+
+
+app.listen(3000, () => console.log("Server running on 3000"))
